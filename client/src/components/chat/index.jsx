2a4ad4b60194b74d88c08fcfae3d3887d8acd7f2
Deepankar Singh <singh.deepankar39@gmail.com>
2023-03-12 14:21:23 +0530

    openai api and login added

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,30 +1,47 @@
-import React from 'react'
-import { useMultiChatLogic,MultiChatSocket,MultiChatWindow } from 'react-chat-engine-advanced'
-import Header from '@/components/customHeader'
-import StandardMessageForm from '@/components/customMessageForms/StandardMessageForm'
+import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
+import Ai from "@/components/customMessageForms/Ai";
+import AiCode from "@/components/customMessageForms/AiCode";
+import AiAssist from "@/components/customMessageForms/AiAssist";
 
-const Chat = () => {
-    const chatProps = useMultiChatLogic(
-        "07fda33f-9f32-4007-b498-e7df0b203033",
-        "test",
-        "12345678"
-    )
+const Chat = ({ user, secret }) => {
+  const chatProps = useMultiChatLogic(
+    "07fda33f-9f32-4007-b498-e7df0b203033",
+    user,
+    secret
+  );
 
   return (
-    <div style={{flexBasis:"100%"}}>
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow 
-         {...chatProps}
-         style={{height:"100vh"}}
-         renderChatHeader={(chat)=> <Header chat={chat}/>} 
-         renderMessageForm={(props)=>{
-          return(
-            <StandardMessageForm props={props} activeChat={chatProps.chat} />
-          )
-         }}
-        />
-    </div>
-  )
-}
+    <div style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+        renderMessageForm={(props) => {
+          if (chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiCode_")) {
+            return <AiCode props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiAssist_")) {
+            return <AiAssist props={props} activeChat={chatProps.chat} />;
+          }
 
-export default Chat
+          return (
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
+        }}
+      />
+    </div>
+  );
+};
+
+export default Chat;
